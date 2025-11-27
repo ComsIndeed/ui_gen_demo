470bd6e0a35d2d597224e8af65182faaa50acc11
Coms <125128463+ComsIndeed@users.noreply.github.com>
2025-11-27 10:48:40 +0800

    refactor: removed streaming_textfield

---
+++ lib/modules/streaming_widget_module/definitions/widget_namespaces.dart
@@ -0,0 +1,9 @@
+class WidgetNamespaces {
+  WidgetNamespaces._();
+
+  static const streamingColumn = "verticalStack";
+  static const streamingRow = "horizontalStack";
+  static const streamingText = "text";
+  static const streamingButton = "button";
+  static const streamingCard = "card";
+}
